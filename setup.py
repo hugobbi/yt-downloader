7a22789710ffdb49ceb84d8becc7af3a292f561e
hugobbi <hgugobbi@gmail.com>
2024-05-30 14:08:14 -0300

    feat: adding setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MP3 YouTube Downloader',
+    author='Henrique Uhlmann Gobbi',
+    author_email='hgugobbi@gmail.com',
+    version=0.1,
+    packages=find_packages(),
+    install_requres=[
+        'yt_dlp',
+        'pydub'
+    ],
+    url='https://github.com/hugobbi/yt-downloader',
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    description='GUI Program to download and trim audio from YouTube videos.'
+)
